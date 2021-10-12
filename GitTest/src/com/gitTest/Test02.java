250ced8fd4f77382b1c30d2607919329d84a8ecd
smhrd <smhrd@DESKTOP-70QF4QM>
2021-10-12 16:36:56 +0900

    세번째 커밋!!

---
+++ GitTest/src/com/gitTest/Test02.java
@@ -0,0 +1,12 @@
+package com.gitTest;
+
+public class Test02 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("기현아 내가 좀 더 잘할게 조장으로써,,,,");
+		System.out.println("옥시시 만세");
+
+	}
+
+}
