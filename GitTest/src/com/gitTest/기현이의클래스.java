7a169854d1b8326d70fe2b073e31f2970a48aa7f
smhrd <smhrd@DESKTOP-6VRK9A5>
2021-10-12 17:21:13 +0900

    기현이 똥멍청이

---
+++ GitTest/src/com/gitTest/기현이의클래스.java
@@ -0,0 +1,17 @@
+package com.gitTest;
+
+import java.util.Scanner;
+
+public class 기현이의클래스 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("이름을 입력하세요 : ");
+		String name = sc.next();
+		
+		System.out.println(name + " 당신은 멍청이 입니다^^");
+	}
+
+}
